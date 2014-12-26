978cd994c3366dc873251ee6089accb1cbe9489a
Ijem <ij.ofili@gmail.com>
2014-12-26 19:17:29 +0000

    added entry

--- appl.js
+++ appl.js
@@ -1,6 +1,7 @@
 var http = require('http');
-http.CreateServer(function (req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+var server = http.CreateServer(function (req, res){
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("BufferStream");
+    res.end();
 }).listen(1337, '127.0.0.1');
 console.log(/*output server message */);
