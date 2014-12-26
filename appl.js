86924856f31962293e0e16c4b2d3c36b8d2ea599
Ijem <ij.ofili@gmail.com>
2014-12-26 19:31:01 +0000

    added entry

--- appl.js
+++ appl.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var server = http.CreateServer(function (req, res){
+var server = http.createServer(function (req, res){
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write("BufferStream");
     res.end();
 })
 
-server.listen(3000);
+server.listen(3090);
