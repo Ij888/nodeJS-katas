87b4bb573bf55b92438d33b3a5c976153013252e
Ijem <ij.ofili@gmail.com>
2014-12-26 12:21:24 +0000

    added entry

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var http = require('http');
-http.createServer(function (req, res){})
-
-
-
-
-
+http.CreateServer(function (req, res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log(/*output server message */);
 
 /*
 var http = require('http');
