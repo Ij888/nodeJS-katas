1d6c78e40cc13c778ff9329126c6a812c23e62eb
Ijem <ij.ofili@gmail.com>
2014-12-26 19:20:10 +0000

    added entry

--- appl.js
+++ appl.js
@@ -3,5 +3,6 @@ var server = http.CreateServer(function (req, res){
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write("BufferStream");
     res.end();
-}).listen(1337, '127.0.0.1');
-console.log(/*output server message */);
+})
+
+server.listen(3000);
