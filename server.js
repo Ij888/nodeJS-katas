323bfbfbebf6453d10822242dae44c2c2681cc61
Ijem <ij.ofili@gmail.com>
2015-01-24 23:47:52 +0000

    draft working files

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ console.log(/*output server message */);
 
 /*
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+http.createServer(200, function(req, res))
+
+
+
+
+
+
+
+
+
+
+
 */
